c76ca65abd09dc7635bb9c04ad0256c41e0e55f7
lwx <zliwanxu@shinewing.club>
2020-04-12 12:30:29 +0800

    index

---
+++ test/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <div v-if="!login">
+      <a href="https://sso.tianwei.pro/user/login?redirect=https://jingtian.pro">登陆</a>
+    </div>
+    <div v-if="login">
+      欢迎回来！您登录的账号为: {{ res.email }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      login: false,
+      res: {}
+    }
+  },
+  mounted() {
+    this.$axios
+      .get('https://sso.tianwei.pro/api/sam/v1/user/info')
+      .then(response => {
+        this.res = response
+      }).catch(error => {
+        if (error.response.status === 401) {
+          this.login = false;
+        }
+      })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ test/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios';
+
+Vue.config.productionTip = false
+Vue.prototype.$axios = axios //全局注册，使用方法为:this.$axios
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
