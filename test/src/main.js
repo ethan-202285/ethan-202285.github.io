6dfaea5dd19759926e5378f25da715a0c8375c72
lwx <zliwanxu@shinewing.club>
2020-04-12 15:32:04 +0800

    with w

--- test/src/main.js
+++ test/src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios';
 
+axios.defaults.withCredentials = true
+
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios //全局注册，使用方法为:this.$axios
 
